988c1cdd7304baace3f124473371e4a50afba071
Sebastian <sebastiancastroramos@gmail.com>
2023-08-31 11:03:25 -0400

    Update PlayView.swift to remove bold lines

--- PlayView.swift
+++ PlayView.swift
@@ -56,7 +56,7 @@ struct PlayView: View {
                         }
                     }
                 }
-                BoldLinesView()
+                // BoldLinesView()
             }
             .padding()
             
