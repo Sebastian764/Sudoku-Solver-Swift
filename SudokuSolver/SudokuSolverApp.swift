ab895aa79110223ff34ee4592c9f79ad3a95d345
Sebastian <scastro2@andrew.cmu.edu>
2023-08-31 10:46:27 -0400

    Add files via upload
    First commit

---
+++ SudokuSolver/SudokuSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolverApp.swift
+//  SudokuSolver
+//
+//  Created by Sebastian Castro on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
